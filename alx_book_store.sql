cd0e3be6350cbb2b3c22a39b59fd706dc6f4b242
philemon <philemonmaosa@gmail.com>
2025-08-17 11:39:40 +0300

    I have made changes

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -26,7 +26,7 @@ CREATE TABLE Orders (
 order_id INT PRIMARY KEY AUTO_INCREMENT, 
 customer_id INT , 
 order_date DATE,
-FOREIGN KEY (customer_id) REFERENCES Customers (customer_id)
+FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 CREATE TABLE Order_Details (
